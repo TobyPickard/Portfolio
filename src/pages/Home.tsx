272aecae01f3300c20ad858c2430f56d4f2fdb19
Toby Pickard <theobzz@yahoo.co.uk>
2024-04-01 13:47:04 +0100

    check

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 // import { useState } from 'react'
-import TopNavbar from "../Components/Navbar"
+import Hello from "./Hello";
 
 function Home() {
     return (
         <>
-            <TopNavbar />
+            <Hello />
         </>
     )
 }
