da635009b0a2f401da5c798d98c12371593c6688
Toby Pickard <theobzz@yahoo.co.uk>
2024-04-01 13:25:13 +0100

    check

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '../../public/vite.svg'
 import './Home.css'
 
 function Home() {
@@ -10,7 +9,6 @@ function Home() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
