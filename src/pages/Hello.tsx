9321f2a6e0a276ec577e0a0285976764ab55b00f
Toby Pickard <theobzz@yahoo.co.uk>
2024-04-01 13:55:59 +0100

    check

--- src/pages/Hello.tsx
+++ src/pages/Hello.tsx
@@ -1,18 +1,18 @@
-import { Button } from "@mui/material";
+// import { Button } from "@mui/material";
 
 const Hello = () => {
-    const handleClick = () => {
-        const element = document.getElementById('projects');
-        if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
+    // const handleClick = () => {
+    //     const element = document.getElementById('projects');
+    //     if (element) {
+    //     element.scrollIntoView({ behavior: 'smooth' });
+    //     }
+    // }
 
     return (
         <div id='hello'>
             <h1 style={{ paddingTop: '50px', display:'flex', justifyContent: 'center', alignItems:'center'}}>Hello</h1>
             <div style={{ display:'flex', justifyContent: 'center', alignItems:'center', paddingBottom: '50px'}}>
-                <Button onClick={handleClick}>Projects</ Button>
+                <button>Testing something</button>
             </div>
         </div>
     );
