26ddedbec4e9230b4032348f7c7395f5c0254b73
Toby Pickard <theobzz@yahoo.co.uk>
2024-04-01 13:36:03 +0100

    check

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './Home.css'
 import TopNavbar from "../Components/Navbar"
 
 function Home() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-        <TopNavbar />
+    return (
         <div>
-            <a href="https://vitejs.dev" target="_blank">
-            </a>
-            <a href="https://react.dev" target="_blank">
-            </a>
+            <TopNavbar />
         </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-            </button>
-            <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-        </div>
-        <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-        </p>
-    </div>
-  )
+    )
 }
 
 export default Home
