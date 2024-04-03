0e37b5fa82f155081729b08ce162ffecc3c8e1c4
Toby Pickard <theobzz@yahoo.co.uk>
2024-04-03 18:06:33 +0100

    making changes to contact

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -13,21 +13,18 @@ const TopNavbar = () => {
         <div style={{ overflowX: 'hidden' }}>
             <AppBar position='static' style={{ zIndex: '100' }}>
                 <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <a href='/' style={{color: 'white'}}>Toby Pickard</ a>
-                </Typography>
-                <Button onClick={() => handleClick('hello')} style={{color: 'white'}}>
-                    Home
-                </Button>
-                <Button onClick={() => handleClick('about')} style={{color: 'white'}}>
-                    About
-                </Button>
-                <Button onClick={() => handleClick('projects')} style={{color: 'white'}}>
-                    Projects
-                </Button>
-                <Button onClick={() => handleClick('contact')} style={{color: 'white'}}>
-                    Contact
-                </Button>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <a href='/' style={{color: 'white'}}>Toby Pickard</ a>
+                    </Typography>
+                    <Button onClick={() => handleClick('hello')} style={{color: 'white'}}>
+                        Home
+                    </Button>
+                    <Button onClick={() => handleClick('about')} style={{color: 'white'}}>
+                        About
+                    </Button>
+                    <Button onClick={() => handleClick('projects')} style={{color: 'white'}}>
+                        Projects
+                    </Button>
                 </Toolbar>
             </AppBar>
         </div>
